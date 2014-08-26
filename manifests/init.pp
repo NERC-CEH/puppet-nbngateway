3edf479cb8ae0cfa6b09e9b64d8d989d55825d24
Christopher Johnson <cjohn@ceh.ac.uk>
2014-08-26 09:22:02 +0100

    Adding the api keys as parameters to the init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,23 +6,27 @@
 #
 # === Parameters
 #
-# [*ssl_cert*]             file location of the ssl certificate
-# [*ssl_key*]              file location of the ssl key certificate
-# [*ssl_chain*]            file location of the chain ssl certificate
-# [*ssl_root*]             file location of the root ssl certificate
+# [*ssl_cert*]              file location of the ssl certificate
+# [*ssl_key*]               file location of the ssl key certificate
+# [*ssl_chain*]             file location of the chain ssl certificate
+# [*ssl_root*]              file location of the root ssl certificate
 #
-# [*vector_shapefiles*]    file location of the vector shapefiles
-# [*os_modern*]            file location of the os modern backdrops
-# [*os_tiled*]             file location of the pregenerated os tiles
+# [*vector_shapefiles*]     file location of the vector shapefiles
+# [*os_modern*]             file location of the os modern backdrops
+# [*os_tiled*]              file location of the pregenerated os tiles
 #
-# [*api_db_password*]      of the api user for use with the api application
-# [*importer_db_password*] of the importer user for use with the importer application
-# [*gis_db_password*]      of the gis user for use with the gis application
-# [*api_db_user*]          username of the api user
-# [*importer_db_user*]     importer of the api user
-# [*gis_db_username*]      gis of the api user
-# [*master_db_sever*]      the servername of the database to use as the master
-# [*environment*]          either developer, staging or production
+# [*api_db_password*]       of the api user for use with the api application
+# [*importer_db_password*]  of the importer user for use with the importer application
+# [*gis_db_password*]       of the gis user for use with the gis application
+# [*api_db_user*]           username of the api user
+# [*importer_db_user*]      importer of the api user
+# [*gis_db_username*]       gis of the api user
+# [*authenticator_key*]     Base64 encoded authenticate key. If not supplied, 
+#   the api web app will generate this.
+# [*credentials_reset_key*] Base64 encoded credentials reset key. Used when 
+#   reseting passwords. If not supplied the api web app will generate this.
+# [*master_db_sever*]       the servername of the database to use as the master
+# [*environment*]           either developer, staging or production
 #
 # === Authors
 #
@@ -39,11 +43,13 @@ class nbngateway (
   $api_db_password,
   $importer_db_password,
   $gis_db_password,
-  $api_db_user         = 'NBNTestAPI',
-  $importer_db_user    = 'NBNImporter',
-  $gis_db_username     = 'NBNGIS',
-  $master_db_sever     = 'nbn-master.nerc-lancaster.ac.uk',
-  $environment         = 'development'
+  $api_db_user           = 'NBNTestAPI',
+  $importer_db_user      = 'NBNImporter',
+  $gis_db_username       = 'NBNGIS',
+  $authenticator_key     = undef,
+  $credentials_reset_key = undef,
+  $master_db_sever       = 'nbn-master.nerc-lancaster.ac.uk',
+  $environment           = 'development'
 ) {
   $warehouse_db_server = $environment ? {
     'production' => 'nbnwarehouse.nerc-lancaster.ac.uk',
